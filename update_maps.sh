898b390decd15421103cb1d81955fefb747243b9
Jonas Walther <go-schiller@t-online.de>
2018-05-16 22:44:12 +0200

    add comment to clear things up with update_maps.sh

--- update_maps.sh
+++ update_maps.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Arctic's script to update/fetch custom maps, currently only a very few amount of maps uploaded.
+# Make sure you gave your public SSH key from your server to @ArcticRevrus#0476
+
 if [ -d "$1" ]
 then
     sftp -a mapsync@getserved.tv:/sftp/maps/*.pk3 $1
