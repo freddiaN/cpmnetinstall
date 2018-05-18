28b71f6fe9e6942afdbb17d93e32dd63b49d676f
Jonas Walther <go-schiller@t-online.de>
2018-05-18 19:51:29 +0200

    + for last commit

--- serverfiles/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-screen -dmS .screenname serverfiles/cnq3-server-x64 +set net_port .port +set sv_punkbuster 0 +set fs_basepath .dir/serverfiles +set dedicated 2 +set com_hunkMegs 512 +exec q3server.cfg +map cpm22 +set fs_game cpma
