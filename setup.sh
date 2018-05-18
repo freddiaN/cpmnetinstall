2ef37d0e80a188c7119cc17fc8d7a24c13c68a16
Jonas Walther <go-schiller@t-online.de>
2018-05-18 19:53:08 +0200

    this should actually work now (except that you will only have stock maps and no pak0.pk3

--- setup.sh
+++ setup.sh
@@ -16,56 +16,56 @@ echo ""
 echo "The script will start executing once you press something"
 read -rsn1
 
-servername="CPMA 1.50 Server"
+SERVERNAME="CPMA 1.50 Server"
 read -p "Please enter the future name of your Server here (default: CPMA 1.50 Server): " newname
-[ -n "$newname" ] && servername=$newname
+[ -n "$newname" ] && SERVERNAME=$newname
 
-read -p "Please enter the port you wish to run the server at (default: 27960): " port
-port=${port:-27960}
+read -p "Please enter the port you wish to run the server at (default: 27960): " PORT
+PORT=${PORT:-27960}
 
-read -p "Please enter a name for the screen session (needed when running multiple servers on the same machine, default: q3): " screenname
-screenname=${screenname:-q3}
+read -p "Please enter a name for the screen session (needed when running multiple servers on the same machine, default: q3): " SCREENNAME
+SCREENNAME=${SCREENNAME:-q3}
 
 read -p "Please enter the directory you want the server to be installed in (default: your home dir): " DIR
 DIR=${1:-$HOME}
 
-read -p "If known, please enter the Country the server is hosted in. (use the 2 character shortcode, use discord's flagemotes for reference): " country
+read -p "If known, please enter the country the server is hosted in. (use the 2 character shortcode, use discord's flagemotes for reference): " COUNTRY
 
-read -p "If known, please enter the state the server is hosted in (mainly ment for the USA): " state
+read -p "If known, please enter the state the server is hosted in (mainly ment for the USA): " STATE
 
-read -p "If known, please enter the City the server is hosted in: " city
+read -p "If known, please enter the City the server is hosted in: " CITY
 
-read -p "Please enter your Discord username (just username, not the id number): " username
-username=${username:-^pfreddia^nn}
+read -p "Please enter your Discord username (just username, not the id number): " USERNAME
+USERNAME=${USERNAME:-^pfreddia^nn}
 
-read -p "Please enter your Discord ID (number after the Hash-sign): " dc_id
-dc_id=${dc_id:-1337}
+read -p "Please enter your Discord ID (number after the Hash-sign): " DC_ID
+DC_ID=${DC_ID:-1337}
 
-read -sp "Please enter the referee password which is used to get admin rights for votes: " refpw
+read -sp "Please enter the referee password which is used to get admin rights for votes: " REFPW
 echo ""
 
-read -sp "Please enter the RCon password used to execute commands on a server level: " rconpw
+read -sp "Please enter the RCon password used to execute commands on a server level: " RCONPW
 echo ""
 
-read -sp "[OPTIONAL] Enter a serverpassword to stop complete randoms to connect to it (nice for PUG focused servers): " serverpw
+read -sp "[OPTIONAL] Enter a serverpassword to stop complete randoms to connect to it (nice for PUG focused servers): " SERVERPW
 echo ""
 
 echo "A recap just to make sure you got everything right:"
 echo ""
-echo "Servername: ${servername}"
-echo "Port: ${port}"
-echo "Screenname: ${screenname}"
+echo "Servername: ${SERVERNAME}"
+echo "Port: ${PORT}"
+echo "Screenname: ${SCREENNAME}"
 echo "Installation directory: ${DIR}"
-echo "Country: ${country}"
-echo "State: ${state}"
-echo "City: ${city}"
-echo "Discord user: ${username} hash ${dc_id}"
-echo "Referee password: ${refpw}"
-echo "RCon password: ${rconpw}"
-echo "Server password (if you didn't set one, this will be blank): ${serverpw}"
-read -r -p "do you want to continue the installation? [y/N] " response
+echo "Country: ${COUNTRY}"
+echo "State: ${STATE}"
+echo "City: ${CITY}"
+echo "Discord user: ${USERNAME} hash ${DC_ID}"
+echo "Referee password: ${REFPW}"
+echo "RCon password: ${RCONPW}"
+echo "Server password (if you didn't set one, this will be blank): ${SERVERPW}"
+read -r -p "do you want to continue the installation? [y/N] " RESPONSE
 
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+if [[ "$RESPONSE" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
     mkdir -p $DIR
     cd $DIR
@@ -76,53 +76,58 @@ then
     TEMP_REPO=$HOME/.temprepoclone
     git clone git@github.com:freddiaN/cpmnetinstall.git --quiet $TEMP_REPO
 
-    # moving it into the DIR
-    mv $TEMP_REPO/init.sh $DIR/init.sh
-    source $DIR/init.sh
+    # execing init script
+    source $TEMP_REPO/init.sh
+
+    # moving things from $TEMP_REPO/serverfiles into main folder
+    mv $TEMP_REPO/start.sh $DIR/start.sh
+    mv $TEMP_REPO/serverfiles/cpma/motd.txt $TEMP_REPO/q3server.cfg $DIR/serverfiles/cpma
+    mv $TEMP_REPO/serverfiles/cpma/cfg-maps/* $DIR/serverfiles/cpma/cfg-maps/
+    mv $TEMP_REPO/serverfiles/baseq3/q3config.cfg $TEMP_REPO/serverfiles/baseq3/q3key $DIR/serverfiles/baseq3/
 
     # Create start script
-    sed -i -e "s/\.screenname/${screenname}/g" start.sh
-    sed -i -e "s/\.port/${port}/g" start.sh
+    sed -i -e "s/\.SCREENNAME/${SCREENNAME}/g" start.sh
+    sed -i -e "s/\.port/${PORT}/g" start.sh
     sed -i -e "s|\.dir|'$DIR'|g" start.sh
     chmod +x start.sh
 
     # setting up q3server.cfg
-    cd serverfiles/baseq3/
-    sed -i -e "s/\.servername/${servername}/g" q3server.cfg
-    sed -i -e "s/\.rconpw/${rconpw}/g" q3server.cfg
-    sed -i -e "s/\.serverpw/${serverpw}/g" q3server.cfg
-    sed -i -e "s/\.refpw/${refpw}/g" q3server.cfg
-    sed -i -e "s/\.country/${country}/g" q3server.cfg
-    sed -i -e "s/\.state/${state}/g" q3server.cfg
-    sed -i -e "s/\.city/${city}/g" q3server.cfg
-    sed -i -e "s/\.un/${username}/g" q3server.cfg
-    sed -i -e "s/\.id/${dc_id}/g" q3server.cfg
+    cd serverfiles/cpma/
+    sed -i -e "s/\.servername/${SERVERNAME}/g" q3server.cfg
+    sed -i -e "s/\.rconpw/${RCONPW}/g" q3server.cfg
+    sed -i -e "s/\.serverpw/${SERVERPW}/g" q3server.cfg
+    sed -i -e "s/\.refpw/${REFPW}/g" q3server.cfg
+    sed -i -e "s/\.country/${COUNTRY}/g" q3server.cfg
+    sed -i -e "s/\.state/${STATE}/g" q3server.cfg
+    sed -i -e "s/\.city/${CITY}/g" q3server.cfg
+    sed -i -e "s/\.un/${USERNAME}/g" q3server.cfg
+    sed -i -e "s/\.id/${DC_ID}/g" q3server.cfg
 
-    if [[ "$state" =~ "" ]]
+    if [[ "$STATE" =~ "" ]]
     then
-        sed -i -e "s/\.state\.country/${country}/g" q3server.cfg
+        sed -i -e "s/\.state\.country/${COUNTRY}/g" q3server.cfg
     else
-        sed -i -e "s/\.state\.country/${state}/g" q3server.cfg
+        sed -i -e "s/\.state\.country/${STATE}/g" q3server.cfg
     fi
 
     # setting up motd.txt
-    if [[ "$state" =~ "" ]]
+    if [[ "$STATE" =~ "" ]]
     then
-        sed -i -e "s/\.state\.country/${country}/g" motd.txt
-        sed -i -e "s/\.city/${city}/g" motd.txt
+        sed -i -e "s/\.state\.country/${COUNTRY}/g" motd.txt
+        sed -i -e "s/\.city/${CITY}/g" motd.txt
     else
-        sed -i -e "s/\.state\.country/${state}/g" motd.txt
-        sed -i -e "s/\.city/${city}/g" motd.txt
+        sed -i -e "s/\.state\.country/${STATE}/g" motd.txt
+        sed -i -e "s/\.city/${CITY}/g" motd.txt
     fi
 
-    sed -i -e "s/\.un/${username}/g" motd.txt
-    sed -i -e "s/\.id/${dc_id}/g" motd.txt
+    sed -i -e "s/\.un/${USERNAME}/g" motd.txt
+    sed -i -e "s/\.id/${DC_ID}/g" motd.txt
 
     # end script
 
     echo ""
     echo "Alright, you're now (nearly) done!"
-    echo "You will still need to manually upload pak0-8.pk3 to ${DIR}/serverfiles/baseq3/"
+    echo "You will still need to manually upload pak0.pk3 to ${DIR}/serverfiles/baseq3/"
     echo "When you are done with that, you can start the server with ./start.sh in ${DIR}."
 else
     exit 0
